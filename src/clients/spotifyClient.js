7035ad1d4617986c53f428d2815897a7193bd234
Sophie LEGRAS <sophielegras@microsoft.com>
2024-09-19 23:48:34 +0200

    Fix path redirect uri

--- src/clients/spotifyClient.js
+++ src/clients/spotifyClient.js
@@ -1,3 +1,3 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
-export const sdk = SpotifyApi.withUserAuthorization("ee9c7a9879ef422fa57741462782e142", window.location.protocol + "//" + window.location.host + "?restoreData=true", ["playlist-modify-public", "ugc-image-upload"]);
+export const sdk = SpotifyApi.withUserAuthorization("ee9c7a9879ef422fa57741462782e142", window.location.protocol + "//" + window.location.host + location.pathname + "?restoreData=true", ["playlist-modify-public", "ugc-image-upload"]);
