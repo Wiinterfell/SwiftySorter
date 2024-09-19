ce1c3f04d073963abc23733314fe18f7124a8e25
Sophie LEGRAS <sophielegras@microsoft.com>
2024-09-19 21:08:44 +0200

    Fix invalid uri

--- src/clients/spotifyClient.js
+++ src/clients/spotifyClient.js
@@ -1,3 +1,3 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
-export const sdk = SpotifyApi.withUserAuthorization("ee9c7a9879ef422fa57741462782e142", window.location.href + "?restoreData=true", ["playlist-modify-public"]);
+export const sdk = SpotifyApi.withUserAuthorization("ee9c7a9879ef422fa57741462782e142", window.location.protocol + "//" + window.location.host + "?restoreData=true", ["playlist-modify-public", "ugc-image-upload"]);
